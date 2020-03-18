f75a82f112e8ecc2b6715bce17fea91769dc2481
Davide <davide.roberti89@gmail.com>
2020-03-18 12:38:03 +0100

    acquisizione input

--- js/main.js
+++ js/main.js
@@ -1,5 +1,19 @@
 $(document).ready(function () {
+    $('button').click(function() {
+        var chiaveRicerca = $('input').val();
+        console.log(chiaveRicerca);
+    });
 
+    // $.ajax({
+    //     url: '',
+    //     method: 'GET',
+    //     success: function (data) {
+    //
+    //     },
+    //     error: function (err) {
+    //
+    //     }
+    // });
 
 
 
